39d4a672f4ef73ee05c2cf6c35788a2563bcf7dd
Yuzhou <836045613@qq.com>
2019-07-17 01:51:36 +0800

    【7.17】Leetcode 爬楼梯，斐波那契数 对称二叉树

---
+++ Assets/BrandoFramework/studystudio/leetcode/IsSymmetric.cs
@@ -0,0 +1,42 @@
+﻿#region Head
+
+// Author:            LinYuzhou
+// Email:             836045613@qq.com
+
+#endregion
+
+using System.Collections.Generic;
+
+namespace Study.LeetCode
+{
+    public partial class Solution
+    {
+        // 给定一个二叉树，检查它是否是镜像对称的。
+        // 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+        //     1
+        //    / \
+        //   2   2
+        //  / \ / \
+        // 3  4 4  3
+        // 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+        //     1
+        //    / \
+        //   2   2
+        //    \   \
+        //    3    3
+
+        public class TreeNode 
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+         }
+        public bool IsSymmetric(TreeNode root)
+        {
+            return true;
+        }
+    }
+}
+
