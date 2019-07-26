6ce256850050fb22cf9c935ed757e23d54fb433a
LinOne1 <836045613@qq.com>
2019-07-26 20:56:07 +0800

    【7.26】Leetcode 两数相加，组合总和 ，字母异位词分组 ，全排列(递归）

--- Assets/BrandoFramework/studystudio/leetcode/Medium/AddTwoNumbers2_445.cs
+++ Assets/BrandoFramework/studystudio/leetcode/Medium/AddTwoNumbers2_445.cs
@@ -116,12 +116,12 @@ namespace Study.LeetCode
             }
             ListNode head=null;
             int flag = 0;
-            while(!stack1.Count == 0||stack2.Count == 0|| flag!=0)
+            while(stack1.Count != 0||stack2.Count == 0|| flag!=0)
             {
                 int value = 0;
-                if(!stack1.Count==0)
+                if(stack1.Count!=0)
                     value+=stack1.Pop();
-                if(!stack2.Count==0)
+                if(stack2.Count!=0)
                     value+=stack2.Pop();
                 value += flag;
                 ListNode node=new ListNode(value%10);
@@ -133,82 +133,6 @@ namespace Study.LeetCode
         }
     #endregion
 
-public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
-{
-    if (l2 == null) return l1;
-    if (l1 == null) return l2;
-    //1.对短链表前位用0补齐
-    ListNode cur1 = l1, cur2 = l2, newListNode = new ListNode(0), newPre = newListNode;
-    //短链表首先到达末尾
-    while (cur1 != null && cur2 != null) 
-    {
-        cur1 = cur1.next;
-        cur2 = cur2.next;
-    }
-    //如果cur2是短链表
-    if (cur1 != null) {
-        //cur1剩余的元素个数就是两个链表的相差长度
-        while (cur1 != null) {
-            //补0
-            newPre.next = new ListNode(0);
-            newPre = newPre.next;
-            cur1 = cur1.next;
-        }
-        //将补上的0接上短链表
-        newPre.next = l2;
-        cur1 = l1;
-        cur2 = newListNode.next;
-    } else if (cur2 != null) {
-        //如果cur1是短链表
-        while (cur2 != null) {
-            //补0
-            newPre.next = new ListNode(0);
-            newPre = newPre.next;
-            cur2 = cur2.next;
-        }
-        //将补上的0接上短链表
-        newPre.next = l1;
-        cur2 = l2;
-        cur1 = newListNode.next;
-    } else {
-        //如果两者等长，则不加0
-        cur1 = l1;
-        cur2 = l2;
-    }
-
-
-    //对等长的两个链表l1/newListNode进行累加，不考虑进位
-    ListNode fakeHead = new ListNode(0), pre = fakeHead;
-    while (cur1 != null) {
-        pre.next = new ListNode(cur1.val + cur2.val);
-        pre = pre.next;
-        cur1 = cur1.next;
-        cur2 = cur2.next;
-    }
-
-    //这是pre位于新链表末尾，从尾往头处理进位
-    int carry = 0;
-    while (pre != fakeHead) {
-        int x = pre.val + carry;
-        carry = x / 10;
-        pre.val = x % 10;
-        //找到pre的前位节点
-        ListNode front = fakeHead;
-        while (front.next != pre) {
-            front = front.next;
-        }
-        pre = front;
-    }
-
-    //判断首位是否需要进位
-    if (carry > 0) 
-    {
-        ListNode tmp = new ListNode(carry);
-        tmp.next = fakeHead.next;
-        fakeHead.next = tmp;
-    }
-    return fakeHead.next;
-}
     }
 }
 
