682ba3978932984bc6dd9cf5dc1e3182d3674a5d
LinOne1 <836045613@qq.com>
2019-08-20 20:48:55 +0800

    【8.20】Leetcode 448. 找到所有数组中消失的数字 461. 汉明距离 337. 打家劫舍 III

--- Assets/BrandoFramework/studystudio/leetcode/Medium/Exist_79.cs
+++ Assets/BrandoFramework/studystudio/leetcode/Medium/Exist_79.cs
@@ -27,7 +27,7 @@ namespace Study.LeetCode
 
 
         private bool[,] hasUseCell;
-        private int[,] directionArray = new int[4,4]{{1,0},{-1,0},{0,1},{0,-1}};
+        private int[,] directionArray = new int[,]{{1,0},{-1,0},{0,1},{0,-1}};
         private int rowCount;
         private int colCount;
         public bool Exist(char[][] board, string word) 
@@ -74,8 +74,8 @@ namespace Study.LeetCode
 
             for(var i = 0;i< 4 ;i++)
             {
-                var newRow = row + directionArray[i][0];
-                var newCol = col + directionArray[i][1];
+                var newRow = row + directionArray[i,0];
+                var newCol = col + directionArray[i,1];
                 if(newRow < rowCount && 
                     newRow >= 0 && 
                     newCol < colCount &&
@@ -96,62 +96,6 @@ namespace Study.LeetCode
             return false;
         }
 
-            private boolean[][] marked;
-
-    //        x-1,y
-    // x,y-1  x,y    x,y+1
-    //        x+1,y
-    private int[][] direction = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-    // 盘面上有多少行
-    private int m;
-    // 盘面上有多少列
-    private int n;
-    private String word;
-    private char[][] board;
-
-    public boolean exist(char[][] board, String word) {
-        m = board.length;
-        if (m == 0) {
-            return false;
-        }
-        n = board[0].length;
-        marked = new boolean[m][n];
-        this.word = word;
-        this.board = board;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dfs(i, j, 0)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean dfs(int i, int j, int start) {
-        if (start == word.length() - 1) {
-            return board[i][j] == word.charAt(start);
-        }
-        if (board[i][j] == word.charAt(start)) {
-            marked[i][j] = true;
-            for (int k = 0; k < 4; k++) {
-                int newX = i + direction[k][0];
-                int newY = j + direction[k][1];
-                if (inArea(newX, newY) && !marked[newX][newY]) {
-                    if (dfs(newX, newY, start + 1)) {
-                        return true;
-                    }
-                }
-            }
-            marked[i][j] = false;
-        }
-        return false;
-    }
-
-    private boolean inArea(int x, int y) {
-        return x >= 0 && x < m && y >= 0 && y < n;
-    }
     }
 }
 
